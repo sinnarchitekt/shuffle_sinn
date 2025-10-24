b82b74bf09c54c512a5a967a5062c8868b7f976f
Fabian Schuller <limone3986@gmail.com>
2025-10-24 23:05:30 +0200

    Fix: Title der Website (Sichtbar im Tab-Reiter) geändert.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ const Index = () => {
   return (
     <Layout> {/* Die Seite in die Layout-Schablone packen */}
       <Head>
-        <title>SensoryCalm \u0028\u2028\u2014\u2028 Homepage</title>
+        <title>Sinnarchitekt</title>
         {/* Den Rest vom Head kannst du hier lassen, falls er seitenspezifisch ist */}
       </Head>
 
